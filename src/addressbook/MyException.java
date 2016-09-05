fcb0903dc2a15848670a1b66af830666d4426f51
Narek <Narek@DESKTOP-OLCFEI4>
2016-09-05 21:35:57 +0400

    commit for commit

--- src/addressbook/MyException.java
+++ src/addressbook/MyException.java
@@ -1,18 +1,15 @@
 package addressbook;
 
-public class MyException extends Exception {
+import java.io.IOException;
+
+public class MyException extends IOException {
 	
 	private static final long serialVersionUID = 4713910484958601732L;
 	
-	public MyException() {
-		
+	public MyException(String e) {
+		super(e);
+//		System.out.println(e + "\n");
+//		printStackTrace();		
 	}
-	
-	
-	
-	
-	
-	
-	
 	
 }
